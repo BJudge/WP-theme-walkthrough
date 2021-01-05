8a9468b881845ac274180282b02c1740d55b8a4f
Bart Judge <bartjudge@Barts-MacBook-Pro.local>
2021-01-05 06:37:22 +0000

    first commit

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php 
+
+    function followAndrew_register_styles() {
+        $version = wp_get_theme()->get('Version');
+        wp_enqueue_style('followAndrew-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', array(), '4.4.1', 'all');
+        wp_enqueue_style('followAndrew-fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css', array(), '5.13.0', 'all');
+        wp_enqueue_style('followAndrew-css-style', get_template_directory_uri() . '/style.css', array('followAndrew-bootstrap'), $version, 'all');
+
+    }
+    
+    add_action('wp_enqueue_scripts', 'followAndrew_register_styles');
+
+    function followAndrew_register_scripts() {
+        wp_enqueue_script('followAndrew-jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js', array(), '3.4.1', true);
+        wp_enqueue_script('followAndrew-popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(), '1.16.0', true);
+        wp_enqueue_script('followAndrew-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(), '4.4.1', true);
+        wp_enqueue_script('followAndrew-mainJS', get_template_directory_uri() . '/assets/js/main.js', array(), '1.0', true);
+    }
+
+    add_action('wp_enqueue_scripts', 'followAndrew_register_scripts');
+
+?>
